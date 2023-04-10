bd5114f8fc3e143184c65199fe775e4201a01c1f
Ahmad Mhaish <ahmad@dconfig.com>
2023-04-10 03:21:05 +0300

    Fixing import problems that prevent running templates

--- bootstrapers/showcase/templates/agency/themeStyles.ts
+++ bootstrapers/showcase/templates/agency/themeStyles.ts
@@ -1,4 +1,5 @@
 import colors from './colors';
+
 export const agencyTheme = ({
   transparent,
   primaryHover,
--- utils/checkIfKnownDomain.ts
+++ utils/checkIfKnownDomain.ts
@@ -1,3 +1,20 @@
+/**
+ * 
+ * @param targetDomain 
+ * @returns The corret domain for the template in case of local development
+ * Supported templates domains as next:
+ * - showcase-app.bcrumbs.net
+ * - showcase-agency.bcrumbs.net
+ * - showcase-saas.bcrumbs.net x-style
+ * - showcase-saas2.bcrumbs.net
+ * - showcase-crypto.bcrumbs.net
+ * - showcase-crypto2.bcrumbs.net x-template
+ * - showcase-charity.bcrumbs.net
+ * - showcase-hosting.bcrumbs.net x-template
+ * - showcase-ride.bcrumbs.net
+ * - showcase-food.bcrumbs.net
+ * 
+ */
 export function checkIfKnownDomain(targetDomain: string) {
   if (
     !targetDomain ||
@@ -6,7 +23,7 @@ export function checkIfKnownDomain(targetDomain: string) {
     targetDomain.indexOf('test.bcrumbs.net') >= 0 ||
     targetDomain.indexOf('test-showcase.bcrumbs.net') >= 0
   ) {
-    return 'showcase-agency.bcrumbs.net';
+    return 'showcase-ride.bcrumbs.net';
   }
 
   return targetDomain;
