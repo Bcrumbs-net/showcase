f316616cfd6032eef37f911e4da1dfb4f4ee413e
Ahmad Mhaish <ahmad@dconfig.com>
2023-08-12 19:21:32 +0300

    Update to catch error on section thumb API

--- pages/api/sectionThumb.ts
+++ pages/api/sectionThumb.ts
@@ -9,31 +9,36 @@ export default async function handler({ query }, res) {
     res.status(404).json({ text: 'Section not found!' });
   }
 
-  const sectionsThumbMap = await fetchTemplateSectionsThumbsMap(
-    +query.templateContextId
-  );
+  try {
+    const sectionsThumbMap = await fetchTemplateSectionsThumbsMap(
+      +query.templateContextId
+    );
 
-  if (
-    sectionsThumbMap &&
-    sectionsThumbMap[query.sectionId] &&
-    sectionsThumbMap[query.sectionId] != 'undefined'
-  ) {
-    res.status(200).json({ text: sectionsThumbMap[query.sectionId] });
-  } else {
-    const url = await renderSectionAsImage({
-      sectionId: query.sectionId,
-      templateId: query.templateId,
-      templateContextId: query.templateContextId,
-    });
-
-    if (url && url !== 'undefined') {
-      updateTemplateSectionThumbFunc({
+    if (
+      sectionsThumbMap &&
+      sectionsThumbMap[query.sectionId] &&
+      sectionsThumbMap[query.sectionId] != 'undefined'
+    ) {
+      res.status(200).json({ text: sectionsThumbMap[query.sectionId] });
+    } else {
+      const url = await renderSectionAsImage({
         sectionId: query.sectionId,
-        contextCompanyId: query.templateContextId,
-        value: url,
+        templateId: query.templateId,
+        templateContextId: query.templateContextId,
       });
-    }
 
-    res.status(200).json({ text: url });
+      if (url && url !== 'undefined') {
+        updateTemplateSectionThumbFunc({
+          sectionId: query.sectionId,
+          contextCompanyId: query.templateContextId,
+          value: url,
+        });
+      }
+
+      res.status(200).json({ text: url });
+    }
+  } catch (ex) {
+    console.log(ex.message);
+    res.status(400).json({ text: 'An error happened in the endpoint.' });
   }
 }
