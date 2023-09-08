919d8656fe17071be3720ece0ef58b4e064c8ab0
Ranim <106822042+RanimHelamiah@users.noreply.github.com>
2023-09-08 22:36:08 +0300

    d

--- lib/organisms/components/testimonials/testimonial1/index.tsx
+++ lib/organisms/components/testimonials/testimonial1/index.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
 import TestimonialSectionWrapper, {
   TextWrapper,
   ImageWrapper,
