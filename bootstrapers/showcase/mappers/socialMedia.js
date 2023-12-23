2ea242886b124324cdec91f9d8b49b5ad6f07836
Ranim <106822042+RanimHelamiah@users.noreply.github.com>
2023-12-24 00:21:39 +0300

    Add mappers for facebook section for all templates

--- bootstrapers/showcase/mappers/socialMedia.js
+++ bootstrapers/showcase/mappers/socialMedia.js
@@ -4,9 +4,44 @@ const resolveComponents = () => {
   return {
     403446: {
       render: props => <FBPageSection {...props} />,
-      name: 'Facebook feeds',
+      name: 'Agency Facebook feeds',
       category: 'Feeds',
     },
+    403541: {
+      render: props => <FBPageSection {...props} />,
+      name: 'App Facebook feeds',
+      category: 'Feeds',
+    },
+    403543: {
+      render: props => <FBPageSection {...props} />,
+      name: 'Saas Facebook feeds',
+      category: 'Feeds',
+    },
+    403542: {
+      render: props => <FBPageSection {...props} />,
+      name: 'Saas2 Facebook feeds',
+      category: 'Feeds',
+    },
+    403544: {
+      render: props => <FBPageSection {...props} />,
+      name: 'Crypto Facebook feeds',
+      category: 'Feeds',
+    },
+    403545: {
+      render: props => <FBPageSection {...props} />,
+      name: 'Charity Facebook feeds',
+      category: 'Feeds',
+    },
+    403546: {
+      render: props => <FBPageSection {...props} />,
+      name: 'Food Facebook feeds',
+      category: 'Feeds',
+    },
+    403547:{
+      render: props => <FBPageSection {...props} />,
+      name: 'Portfolio Facebook feeds',
+      category: 'Feeds',
+    }
   };
 };
 export default resolveComponents;
