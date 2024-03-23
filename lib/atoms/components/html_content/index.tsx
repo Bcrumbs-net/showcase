8dda71ecd3b59f93e47526e060de4d95f85c9ea7
Ahmad Mhaish <ahmad@dconfig.com>
2024-03-23 14:52:09 +0300

    Updating default height for html content

--- lib/atoms/components/html_content/index.tsx
+++ lib/atoms/components/html_content/index.tsx
@@ -5,7 +5,7 @@ const Container = styled.div<{ height?: string }>`
   width: 100%;
   margin-top: 10px;
   margin-bottom: 10px;
-  height: ${(props) => props.height || '150px'};
+  height: ${(props) => props.height || '400px'};
   overflow-y: auto;
   border: 1px solid #ccc;
   border-radius: 5px;
