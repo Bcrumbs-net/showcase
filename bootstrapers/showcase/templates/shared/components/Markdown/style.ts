1e9a8b8a77bd9e6c04b73bbc00c49f583a333338
Ahmad Mhaish <ahmad@dconfig.com>
2025-01-05 18:28:49 +0300

    Updates on Markdown styling

--- bootstrapers/showcase/templates/shared/components/Markdown/style.ts
+++ bootstrapers/showcase/templates/shared/components/Markdown/style.ts
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
 const MarkdownPageSectionWrapper = styled.section`
-  max-width: 1000px;
+  max-width: 720px;
   margin: 0 auto;
-  padding: 120px 0;
+  padding: 120px 20px;
   overflow: hidden;
   line-height: 2em;
 
   @media (max-width: 990px) {
-    padding: 80px 0;
+    padding: 80px 20px;
   }
   @media (max-width: 767px) {
-    padding: 70px 0 60px 0;
+    padding: 70px 20px 60px;
   }
 
   h1, p {
